d50e658f25c25eb55304f990af489f7cf48de9be
warlord97 <88074251+warlord97@users.noreply.github.com>
2023-08-18 18:49:02 +0530

    changes

--- index.js
+++ index.js
@@ -25,19 +25,19 @@ app.use(express.json({limit: "50mb"}));
 app.use(morgan("common"));
 app.use(cookieParser());
 
-let origin = "http://localhost:3000";
-console.log("here env is ", process.env.NODE_ENV);
-console.log('the origin url is ',origin)
-if (process.env.NODE_ENV == 'production') {
-  console.log('I am inside production');
-  origin = process.env.CORS_ORIGIN;
-  console.log('origin url is ',origin);
-}
+// let origin = "http://localhost:3000";
+// console.log("here env is ", process.env.NODE_ENV);
+// console.log('the origin url is ',origin)
+// if (process.env.NODE_ENV == 'production') {
+//   console.log('I am inside production');
+//   origin = process.env.CORS_ORIGIN;
+//   console.log('origin url is ',origin);
+// }
 
 app.use(
   cors({
     credentials: true,
-    origin,
+    origin: process.env.CORS_ORIGIN,
   })
 );
 
